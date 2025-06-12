6a6a2d721fc43ba99aadbccee27e51e6b5e9beb2
Keivalya Pandya <61532731+keivalya@users.noreply.github.com>
2025-06-12 13:40:47 +0000

    added fibonacci

--- Algorithms/recursion.py
+++ Algorithms/recursion.py
@@ -1,4 +1,5 @@
-# factorial using recursion
+# Factorial
+# ... using recursion
 def findFactorialRecursive(num):
     if num == 1:
         return 1
@@ -12,3 +13,17 @@ def findFactorialIterative(num):
     for i in range(1, num+1):
         res = res * i
     return res
+
+# Fibonacci
+# ... using recursion
+def fibonacciRecursive(n):
+    if n < 2:
+        return n
+    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+
+# ... using iterative
+def fibonacciIterative(n):
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib[n]
